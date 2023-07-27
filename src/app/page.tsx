c1608134081ef14418ef233cd380c70fc1153c9c
cloud <cloud@oysterable.com>
2023-07-27 15:51:13 +0900

    [Add] Install Tailwind css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main>
-      Home!
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Link href="yoyo">Go Yoyo</Link>
       <Link href="/dynamic/test">Go Dynamic</Link>
     </main>
