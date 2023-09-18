246ba3389b44935e95971fc9485f44a5c43ef8c4
cloud <cloud@oysterable.com>
2023-09-18 21:31:02 +0900

    [Add] 사이드바 외부 클릭시 자동 닫힘

--- src/app/components/layout/BasicLayout.tsx
+++ src/app/components/layout/BasicLayout.tsx
@@ -6,21 +6,37 @@ import Sidebar from '../SideBar';
 import './BasicLayout.css';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useOutsideClick } from '@/app/hooks/useTest';
+import { useRouter } from 'next/navigation';
 
 const BasicLayout: React.FC<PropsWithChildren<{}>> = ({ children }) => {
+  const route = useRouter();
   const [isToggle, setIsToggled] = React.useState<boolean>(false);
 
   const handleToggle = (visibility: boolean) => {
     setIsToggled(visibility);
   };
 
+  const sideBarRef = useOutsideClick(() => {
+    isToggle && handleToggle(false);
+  });
+
   return (
     <>
       <header className="header_container">
         <BurgerButton isToggled={isToggle} handleToggle={handleToggle} />
-        <Image src="/muscle-logo.svg" alt="logo" width={42} height={36} />
+        <Image
+          className="icon"
+          src="/muscle-logo.svg"
+          alt="logo"
+          width={42}
+          height={36}
+          onClick={() => route.push('/')}
+        />
       </header>
-      <Sidebar isVisible={isToggle} handleClicked={handleToggle} />
+      <div ref={sideBarRef}>
+        <Sidebar isVisible={isToggle} handleClicked={handleToggle} />
+      </div>
       <main className="main_container">{children}</main>
       <footer className="footer_container text-base font-bold	">
         A project by{' '}
--- src/app/hooks/useTest.ts
+++ src/app/hooks/useTest.ts
@@ -1,7 +1,23 @@
-import React from 'react';
+'use client';
 
-const useTest = () => {
-  return [];
+import { useRef, useEffect } from 'react';
+
+export const useOutsideClick = (callback: () => void) => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        callback();
+      }
+    };
+
+    document?.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document?.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [callback]);
+
+  return ref;
 };
-
-export default useTest;
