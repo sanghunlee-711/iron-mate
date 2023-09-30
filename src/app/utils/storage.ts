fe9f97e93c72e807a1c7421ee511e0872360b729
cloud <cloud@oysterable.com>
2023-09-30 10:58:40 +0900

    [Modify] storage 클래스 window추가

--- src/app/utils/storage.ts
+++ src/app/utils/storage.ts
@@ -9,25 +9,27 @@ class DataStorage {
 
   set(key: string = 'iron-mate-data', data: any) {
     try {
-      localStorage.setItem(key, JSON.stringify(data));
+      window?.localStorage.setItem(key, JSON.stringify(data));
       this.customAlert.toast('브라우저 내 데이터 저장이 완료되었습니다.');
     } catch (e) {
       this.customAlert.toast('브라우저 내 데이터 저장에 실패하였습니다.');
       console.error(e);
     }
   }
+
   get(key: string = 'iron-mate-data') {
     try {
-      const data = JSON.parse(localStorage.getItem(key) as string);
+      const data = JSON.parse(window?.localStorage.getItem(key) as string);
       return data;
     } catch (e) {
       this.customAlert.toast('브라우저 내 데이터 가져오기에 실패하였습니다.');
       console.error(e);
     }
   }
+
   remove(key: string = 'iron-mate-data') {
     try {
-      const data = localStorage.removeItem(key);
+      const data = window?.localStorage.removeItem(key);
       return data;
     } catch (e) {
       this.customAlert.toast('브라우저 내 데이터 삭제하기에 실패하였습니다.');
@@ -35,4 +37,5 @@ class DataStorage {
     }
   }
 }
+
 export default DataStorage;
