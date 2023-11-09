dda067644dfa2238b9a955052df4599338f425b8
sanghunlee-711 <cloudlee711@gmail.com>
2023-11-09 14:36:39 +0900

    [Add] Sentry 세팅

--- next.config.js
+++ next.config.js
@@ -1,10 +1,29 @@
 /** @type {import('next').NextConfig} */
 // dev warning : https://github.com/GoogleChrome/workbox/issues/1790
-const prod = process.env.NODE_ENV === 'production';
+const prod = process.env.NODE_ENV === "production";
+const { withSentryConfig } = require("@sentry/nextjs");
 
-const withPWA = require('next-pwa')({
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, configFile, stripPrefix, urlPrefix, include, ignore
+
+  org: "The-Folks",
+  project: "iron-mate",
+
+  // An auth token is required for uploading source maps.
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+
+  silent: true, // Suppresses all logs
+
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+const nextConfig = require("next-pwa")({
   disable: prod ? false : true,
-  dest: 'public',
+  dest: "public",
 });
 
-module.exports = withPWA;
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
