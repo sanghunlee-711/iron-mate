21f248f5e865a2655aede0dce026dec7dc429c39
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2023-11-09 14:43:20 +0900

    [38] 센트리 세팅 (#39)
    * [#19] IOS에서 아이콘 배경이 투명할때 검정으로 변하는 현상 (#37)
    
    * [Modify] Apple Touch Icon setting
    
    * [#19] PWA Setting > Apple icon setting
    
    * [Fix] IOS에서 다운로드 하는 경우 아이콘 배경이 투명할때 검정으로
    아이콘이 변하는 현상
    
    * [Add] Sentry 세팅

---
+++ src/app/utils/storage.ts
@@ -0,0 +1,43 @@
+import CustomAlert from './alert';
+
+class DataStorage {
+  customAlert;
+
+  constructor() {
+    this.customAlert = new CustomAlert();
+  }
+
+  set(key: string = 'iron-mate-data', data: any) {
+    try {
+      global?.window?.localStorage.setItem(key, JSON.stringify(data));
+      this.customAlert.toast('브라우저 내 데이터 저장이 완료되었습니다.');
+    } catch (e) {
+      this.customAlert.toast('브라우저 내 데이터 저장에 실패하였습니다.');
+      console.error(e);
+    }
+  }
+
+  get(key: string = 'iron-mate-data') {
+    try {
+      const data = JSON.parse(
+        (global?.window?.localStorage.getItem(key) || '') as string
+      );
+      return data;
+    } catch (e) {
+      this.customAlert.toast('브라우저 내 데이터 가져오기에 실패하였습니다.');
+      console.error(e);
+    }
+  }
+
+  remove(key: string = 'iron-mate-data') {
+    try {
+      const data = global?.window?.localStorage.removeItem(key);
+      return data;
+    } catch (e) {
+      this.customAlert.toast('브라우저 내 데이터 삭제하기에 실패하였습니다.');
+      console.error(e);
+    }
+  }
+}
+
+export default DataStorage;
